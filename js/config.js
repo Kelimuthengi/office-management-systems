a96db494620fb8b0275b763170b4d7ccfb9b8fd1
Jimmy Muthengi <theeoligarchy@gmail.com>
2021-11-01 09:20:26 +0300

    first commit

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// api key
+const firebaseConfig = {
+    apiKey: "AIzaSyDkObDYD3no81VsKPe4mpBGkqaTJd3cky8",
+    authDomain: "office-management-b7f90.firebaseapp.com",
+    projectId: "office-management-b7f90",
+    storageBucket: "office-management-b7f90.appspot.com",
+    messagingSenderId: "690627937453",
+    appId: "1:690627937453:web:169b9a22b16bbe7c72b224",
+    measurementId: "G-HM2ZJFX9LY"
+  };
+
+//   initailizing
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ js/register.js
@@ -0,0 +1,21 @@
+document.getElementById("signup").onclick = function() {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+
+    // sign up the user
+    firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
+
+        var user = userCredential.user;
+
+    }).catch( (error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+    })
+    
+
+    
+}
